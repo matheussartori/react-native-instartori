55392b12a1f4c76cf7eb90dcec120ac8f80de49d
Matheus Sartori <ms.sartori@outlook.com.br>
2018-08-08 23:45:42 -0300

    Usando componentes de lista <FlatList>

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Dimensions, FlatList} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
@@ -19,15 +19,17 @@ export default class App extends Component<Props> {
       ,{id: 3, usuario: 'victor'}];
 
     return (
-        <View>
-        {fotos.map(foto =>
-            <View key={foto.id}>
-                <Text>{foto.usuario}</Text>
+        <FlatList
+            keyExtractor={item => String(item.id)}
+            data={fotos}
+            renderItem={ ({item}) =>
+            <View>
+                <Text>{item.usuario}</Text>
                 <Image source={require('./resources/img/alura.jpg')}
                     style={{width: width, height: width}}/>
             </View>
-        )}
-        </View>
+            }
+        />
     );
   }
 }
