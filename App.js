0c941d57fba49d3f5a65c34a7cc04cd853969a38
Matheus Sartori <ms.sartori@outlook.com.br>
2018-08-08 23:32:53 -0300

    Exibindo várias fotos com array fixo

--- App.js
+++ App.js
@@ -14,11 +14,19 @@ const width = Dimensions.get('screen').width;
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+      const fotos = [{id: 1, usuario: 'matheus'}
+      ,{id: 2, usuario: 'ronaldo'}
+      ,{id: 3, usuario: 'victor'}];
+
     return (
         <View>
-            <Text>Matheus</Text>
-            <Image source={require('./resources/img/alura.jpg')}
-                style={{width: width, height: width}}/>
+        {fotos.map(foto =>
+            <View key={foto.id}>
+                <Text>{foto.usuario}</Text>
+                <Image source={require('./resources/img/alura.jpg')}
+                    style={{width: width, height: width}}/>
+            </View>
+        )}
         </View>
     );
   }
