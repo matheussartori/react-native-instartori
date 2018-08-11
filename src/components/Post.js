226b51a38beee513b6cc28c507a4cec51578c746
Matheus Sartori <ms.sartori@outlook.com.br>
2018-08-11 00:14:11 -0300

    Botão de like reagindo ao toque

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,52 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Dimensions, FlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Dimensions, FlatList, TouchableOpacity} from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        }
+    }
+
+    loadIcon(liked) {
+        if(liked == 1) {
+            return require('../../resources/img/s2-checked.png');
+        } else {
+            return require('../../resources/img/s2.png');
+        }
+    }
+
+    like() {
+        const updatedFoto = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada
+        }
+
+        this.setState({foto: updatedFoto})
+    }
+
     render() {
+        const { foto } = this.state;
+
         return (
             <View>
                 <View style={styles.header}>
-                    <Image source={{uri: this.props.foto.profile_photo}}
-                        style={styles.profilePhoto}/>
-                    <Text>{this.props.foto.login}</Text>
+                    <Image source={{uri: foto.profile_photo}}
+                        style={styles.profilePhoto} />
+                    <Text>{foto.login}</Text>
                 </View>
-                <Image source={{uri: this.props.foto.url}}
+                <Image source={{uri: foto.url}}
                     style={styles.postPhoto}/>
+
+                <View style={styles.footer}>
+                    <TouchableOpacity onPress={this.like.bind(this)}>
+                        <Image style={styles.likeButton}
+                            source={this.loadIcon(foto.likeada)} />
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -34,5 +67,12 @@ const styles = StyleSheet.create({
     postPhoto: {
         width: width,
         height: width
+    },
+    likeButton: {
+        width: 40,
+        height: 40
+    },
+    footer: {
+        margin: 10
     }
 });
